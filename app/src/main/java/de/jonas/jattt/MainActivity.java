ded9ea3f696717b24212089823ae1ad187e57edc
Jonas Lobe <jonas.lobe.2006@outlook.com>
2021-12-11 21:07:49 +0100

    Initial commit

---
+++ app/src/main/java/de/jonas/jattt/MainActivity.java
@@ -0,0 +1,13 @@
+package de.jonas.jattt;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public final class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
