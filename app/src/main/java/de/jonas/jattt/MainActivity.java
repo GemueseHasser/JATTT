f029a5cf4b4ca670b09db3a13fdb6c608c28fbd9
Jonas Lobe <jonas.lobe.2006@outlook.com>
2021-12-11 21:14:01 +0100

    implement play button

--- app/src/main/java/de/jonas/jattt/MainActivity.java
+++ app/src/main/java/de/jonas/jattt/MainActivity.java
@@ -1,13 +1,28 @@
 package de.jonas.jattt;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
 
-public final class MainActivity extends AppCompatActivity {
+public final class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button play = super.findViewById(R.id.play_button);
+        play.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(final View view) {
+
+    }
+
+    @Override
+    public void onPointerCaptureChanged(final boolean hasCapture) {
+        super.onPointerCaptureChanged(hasCapture);
     }
 }
